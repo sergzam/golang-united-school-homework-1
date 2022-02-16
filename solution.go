536f0f87554cb9972fedccd387a2ce3162a32bac
Sergii Zamashnykh <Sergii.Zamashnykh@refinitiv.com>
2022-02-16 14:09:31 +0200

    hope it will work in this branch

--- solution.go
+++ solution.go
@@ -1,5 +1,8 @@
 package solution
 
+import "github.com/kyokomi/emoji/v2"
+
+//goland:noinspection GoUnusedExportedFunction
 func GetMessage() string {
-	return ""
+	return emoji.Sprint("Hello :world_map:!")
 }
